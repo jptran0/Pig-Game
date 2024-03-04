2e37519e597762da23625ef4bf8b2789ae10a3dd
Jazmine Tran <jazminetra00@gmail.com>
2024-03-04 16:54:11 -0600

    new button is functional and created init function

--- script.js
+++ script.js
@@ -13,15 +13,33 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  // Starting conditions
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  diceEl.classList.add('hidden');
+
+  // Resets scores
+  score0El.textContent = scores[0];
+  score1El.textContent = scores[1];
+
+  // Resets current scores
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  // Removes winner
+  player0El.classList.remove('player--winner', 'player--active');
+  player0El.classList.add('player--active');
+  activePlayer = 0;
+
+  player1El.classList.remove('player--winner', 'player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -79,3 +97,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
